8f4d4dc52db809206a0d7fb6e0427641d246325e
lucasfarmwald <farmwaldlucas@gmail.com>
2022-04-02 18:19:29 -0500

    made changes

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,16 @@
 class CashRegister
-    
-    def initialize(employee_discount)
+    attr_accessor :discount, :total, :quantity
+    def initialize(discount = 0)
         @total = 0
+        @discount = discount
         
     end
     def total
-       
+       @total
     end
-    def add_item=(title, price)
-
-
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+        
     end
     def apply_discount
     end
