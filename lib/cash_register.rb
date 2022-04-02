f34c813f2cff4d875213c9b2d23f20db0f634bf7
lucasfarmwald <farmwaldlucas@gmail.com>
2022-04-02 17:17:10 -0500

    init commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
+    
+    def initialize(employee_discount)
+        @total = 0
+        
+    end
+    def total
+       
+    end
+    def add_item=(title, price)
 
+
+    end
+    def apply_discount
+    end
+    def items
+
+    end
+    def void_last_transaction
+
+    end
+end
