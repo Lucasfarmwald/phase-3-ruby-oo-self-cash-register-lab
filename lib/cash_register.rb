c4a05f876e466e3b7f86ff67aa0882ebb2ccad3d
lucasfarmwald <farmwaldlucas@gmail.com>
2022-04-03 13:40:01 -0500

    changed apply discount method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,14 +3,19 @@ class CashRegister
     def initialize(discount = 0)
         @total = 0
         @discount = discount
+        
     end
     def total
        @total
     end
     def add_item(title, price, quantity = 1)
         self.total += price * quantity
+        
     end
     def apply_discount
+       if @discount
+        self.total = self
+
     end
     def items
 
